070b46cb0dbc87fd7526ed596f9ff68c21874b5f
Kamil Sygut <k.sygut@bloomga.com>
2020-04-07 17:49:55 +0200

    31. Making HTTP Requests

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=084faa9609f60350e65abf5ad4d4dd02&query=37.8267,-122.4233'
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
-
-console.log('Stopping')
+request({ url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
