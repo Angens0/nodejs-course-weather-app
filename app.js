16d260fbc4ec904d596d0d132c4137d6217cddfb
Kamil Sygut <k.sygut@bloomga.com>
2020-04-08 13:36:50 +0200

    34. Handling Errors

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const request = require('request')
 const url = 'http://api.weatherstack.com/current?access_key=084faa9609f60350e65abf5ad4d4dd02&query=37.8267,-122.4233'
 
 request({ url, json: true }, (error, response) => {
+    if(error) {
+        console.log('Unable to connect to weather service!')
+        return
+    } else if(response.body.error) {
+        console.log('Unable to find location')
+        return
+    }
+
     const {weather_descriptions, temperature, feelslike} = response.body.current
     console.log(`${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees`)
 })
@@ -12,6 +20,14 @@ const geocodeSearchText = 'los angeles'
 const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${geocodeSearchText}.json?access_token=${geocodeToken}&limit=1`
 
 request({ url: geocodeURL, json: true }, (error, response) => {
+    if(error) {
+        console.log('Unable to connect to location services!')
+        return
+    } else if(response.body.error || response.body.message === 'Not Found' || !response.body.features.length) {
+        console.log('Unable to find location')
+        return
+    }
+
     const [longitude, latitude] = response.body.features[0].center
     console.log(longitude, latitude)
 })
