de96346643e6f245723a1eb1e7b6b8140d1fc120
Kamil Sygut <k.sygut@bloomga.com>
2020-04-08 04:08:12 +0200

    33. An HTTP Request Challenge

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ request({ url, json: true }, (error, response) => {
     const {weather_descriptions, temperature, feelslike} = response.body.current
     console.log(`${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees`)
 })
+
+const geocodeToken = 'pk.eyJ1IjoiYW5nZW5zMCIsImEiOiJjazhxbHJmcHUwNDlqM3BubGkyb203cmUxIn0.MPiPGwPNCL09ufaLIEllrg'
+const geocodeSearchText = 'los angeles'
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${geocodeSearchText}.json?access_token=${geocodeToken}&limit=1`
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    const [longitude, latitude] = response.body.features[0].center
+    console.log(longitude, latitude)
+})
